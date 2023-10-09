8c47544d5294e537b450fc244c28e8bc164c5e87
Nguyen Trung <nguyentrung@Trungs-iMac.local>
2023-10-09 23:59:32 +0700

    WIP: leaders

--- src/routes/leader/schema-api.js
+++ src/routes/leader/schema-api.js
@@ -2,19 +2,52 @@ const Joi = require('joi').extend(require('@joi/date'))
 Joi.objectId = require('joi-objectid')(Joi)
 
 const model = {
-    name: Joi.string().max(256),
-    description: Joi.string().max(1000),
+    name: Joi.string().max(1000),
+    caption: Joi.string().max(1000),
+    departmentId: Joi.objectId(),
+    positionId: Joi.objectId(),
+    badgeId: Joi.objectId(),
+    folderId: Joi.objectId(),
+    periodStart: Joi.date(),
+    periodEnd: Joi.date(),
+    orderNum: Joi.number(),
+    transcript: Joi.string().max(10000),
+    videoUrl: Joi.string(),
+    imageUrl: Joi.string(),
+    audioUrl: Joi.string(),
 }
 
 const updateBody = Joi.object({
-    name: Joi.string().max(256),
-    description: Joi.string().max(1000),
+    name: model.name,
+    caption: model.caption,
+    departmentId: model.departmentId,
+    positionId: model.positionId,
+    badgeId: model.badgeId,
+    folderId: model.folderId,
+    periodStart: model.periodStart,
+    periodEnd: model.name,
+    orderNum: model.orderNum,
+    transcript: model.transcript,
+    videoUrl: model.videoUrl,
+    imageUrl: model.imageUrl,
+    audioUrl: model.audioUrl,
 })
 
 const post = {
     body: Joi.object({
         name: model.name.required(),
-        description: model.description,
+        departmentId: model.departmentId.required(),
+        positionId: model.positionId.required(),
+        badgeId: model.badgeId.required(),
+        folderId: model.folderId.required(),
+        periodStart: model.periodStart,
+        periodEnd: model.name,
+        caption: model.caption,
+        orderNum: model.orderNum,
+        transcript: model.transcript,
+        videoUrl: model.videoUrl,
+        imageUrl: model.imageUrl.required(),
+        audioUrl: model.audioUrl,
     }),
 }
 
