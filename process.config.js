8a7cb5b51eee500f452a9fc2e685cb5e9a75741f
Nguyen Trung <nguyentrung@Nguyens-iMac.local>
2023-10-18 09:30:02 +0700

    update process.config.js

--- process.config.js
+++ process.config.js
@@ -12,63 +12,6 @@ const appDefs = [
         max_memory_restart: '1G',
         node_args: '--expose-gc --max-old-space-size=1024',
     },
-    {
-        // node_args: ['--inspect', '--debug=0.0.0.0:7000'],
-        name: 'api-qas',
-        script: './src/start.js',
-        env: {
-            NODE_ENV: 'preprod',
-        },
-        instances: 1,
-        max_memory_restart: '512M',
-        node_args: '--expose-gc --max-old-space-size=512',
-        exec_mode: 'cluster',
-    },
-    {
-        // node_args: ['--inspect', '--debug=0.0.0.0:7000'],
-        name: 'api',
-        script: './src/start.js',
-        env: {
-            NODE_ENV: 'staging',
-        },
-        instances: 1,
-        max_memory_restart: '512M',
-        node_args: '--expose-gc --max-old-space-size=512',
-        exec_mode: 'cluster',
-    },
-    {
-        name: 'cronjob',
-        script: './src/cronjob.js',
-        env: {
-            NODE_ENV: 'production',
-        },
-        instances: 1,
-        exec_mode: 'fork',
-        max_memory_restart: '1G',
-        node_args: '--expose-gc --max-old-space-size=1024',
-    },
-    {
-        name: 'cronjob-qas',
-        script: './src/cronjob.js',
-        env: {
-            NODE_ENV: 'preprod',
-        },
-        instances: 1,
-        exec_mode: 'fork',
-        max_memory_restart: '512M',
-        node_args: '--expose-gc --max-old-space-size=512',
-    },
-    {
-        name: 'cronjob',
-        script: './src/cronjob.js',
-        env: {
-            NODE_ENV: 'staging',
-        },
-        instances: 1,
-        exec_mode: 'fork',
-        max_memory_restart: '512M',
-        node_args: '--expose-gc --max-old-space-size=512',
-    },
 ]
 
 console.log('PM2 Environment', process.env.NODE_ENV)
