ce899917663d16fb5f8b1d712a934a7516d00ae8
Nguyen Trung <nguyentrung@Nguyens-iMac.local>
2023-10-18 09:30:12 +0700

    update process.config.js

--- process.config.js
+++ process.config.js
@@ -12,6 +12,18 @@ const appDefs = [
         max_memory_restart: '1G',
         node_args: '--expose-gc --max-old-space-size=1024',
     },
+    {
+        // node_args: ['--inspect', '--debug=0.0.0.0:7000'],
+        name: 'api',
+        script: './src/start.js',
+        env: {
+            NODE_ENV: 'staging',
+        },
+        instances: 1,
+        max_memory_restart: '512M',
+        node_args: '--expose-gc --max-old-space-size=512',
+        exec_mode: 'cluster',
+    },
 ]
 
 console.log('PM2 Environment', process.env.NODE_ENV)
